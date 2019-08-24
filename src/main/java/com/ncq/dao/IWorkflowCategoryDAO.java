2c018eb70ec8723284022ad49a9eb15ba1384aad
mmelki <mmelki@iobeya.com>
2019-08-24 12:51:24 +0200

    Add workflow category dao

---
+++ src/main/java/com/ncq/dao/IWorkflowCategoryDAO.java
@@ -0,0 +1,8 @@
+package com.ncq.dao;
+
+import com.ncq.model.WorkflowCategory;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IWorkflowCategoryDAO extends IGenericDAO<WorkflowCategory, String> {
+}
