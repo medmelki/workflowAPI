b6e66b285bc1b469e68f4851b0f50ac9f747d734
mmelki <mmelki@iobeya.com>
2019-08-25 19:55:33 +0200

    Enrich integration test

--- src/test/java/com/ncq/dao/WorkflowDAOIT.java
+++ src/test/java/com/ncq/dao/WorkflowDAOIT.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class WorkflowDAOIT extends AbstractDAOIT {
 
@@ -26,11 +28,47 @@ public class WorkflowDAOIT extends AbstractDAOIT {
     }
 
     @Test
-    public void getWorkflowsWithoutFilters() {
+    public void getWorkflowsByFiltersTest() {
 
+        // test retrieval without filters
         List<Workflow> workflowList = workflowDAO.getWorkflowsByFilters(null, null, 0, 0, 10);
 
+        assertNotNull(workflowList);
         assertEquals(5, workflowList.size());
+
+
+        // test retrieval with name filter
+        workflowList = workflowDAO.getWorkflowsByFilters("workflow1", null, 0, 0, 10);
+
+        assertNotNull(workflowList);
+        assertEquals(1, workflowList.size());
+
+
+        // test retrieval with status filter
+        workflowList = workflowDAO.getWorkflowsByFilters(null, null, 1, 0, 10);
+
+        assertNotNull(workflowList);
+        assertEquals(2, workflowList.size());
+
+        // test retrieval with category ids filter
+        workflowList = workflowDAO.getWorkflowsByFilters(null, asList(CATEGORY_ID_2, CATEGORY_ID_4), 1, 0, 10);
+
+        assertNotNull(workflowList);
+        assertEquals(2, workflowList.size());
+
+
+        // test name, status and category ids filters combined with empty result
+        workflowList = workflowDAO.getWorkflowsByFilters("workflow6", asList(CATEGORY_ID_1, CATEGORY_ID_2, CATEGORY_ID_3, CATEGORY_ID_4), 1, 0, 10);
+
+        assertNotNull(workflowList);
+        assertEquals(0, workflowList.size());
+
+
+        // test name, status and category ids filters combined with empty result
+        workflowList = workflowDAO.getWorkflowsByFilters("workflow1", asList(CATEGORY_ID_1, CATEGORY_ID_2, CATEGORY_ID_3, CATEGORY_ID_4), 1, 0, 10);
+
+        assertNotNull(workflowList);
+        assertEquals(1, workflowList.size());
     }
 
 
